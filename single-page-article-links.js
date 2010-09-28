9cc9265eea7329b73e714063b52a95fb9a0ca19d
jasonphanley <jasonphanley@gmail.com>
2010-09-28 17:14:49 -0400

    Update regex's to handle a dash in the URL.

--- single-page-article-links.js
+++ single-page-article-links.js
@@ -1,4 +1,4 @@
-var articleUrlRegEx = /^https?:\/\/(?:www|travel)\.nytimes\.com\/(?:(?:aponline|reuters)\/)?\d{4}\/\d{2}\/\d{2}(?:\/\w+)*\/[-\w]+\.html(\?[^#]*)?(#.*)?$/
+var articleUrlRegEx = /^https?:\/\/(?:www|travel)\.nytimes\.com\/(?:(?:aponline|reuters)\/)?\d{4}\/\d{2}\/\d{2}(?:\/[-\w]+)*\/[-\w]+\.html(\?[^#]*)?(#.*)?$/
 var articleUrlRegExMatcher = new RegExp(articleUrlRegEx)
 
 singlePageArticleLinks(document.links);
